155f66e4d3b1c290dffd2e12da4b1591fc300d40
Aslan <isip_a.g.asalbekov@mpt.ru>
2023-09-26 16:58:04 +0300

    Revert "nice"
    This reverts commit 351e6d800b771d1f26050947cc1bc450969915d7.

--- main/helloworld.go
+++ main/helloworld.go
@@ -19,17 +19,14 @@ type Worker struct {
 type WorkerList struct {
     Workers []Worker
 }
-var db *sql.DB 
 
 func main() {
     connStr := "host=localhost port=5433 dbname=postgres user=postgres password=123 sslmode=disable"
-    var err error
-    db, err = sql.Open("postgres", connStr) 
+    db, err := sql.Open("postgres", connStr)
     if err != nil {
         panic(err)
     }
     defer db.Close()
-
 
     rows, err := db.Query("select * from Worker")
     if err != nil {
@@ -53,7 +50,6 @@ func main() {
         fmt.Println("Действие:")
         fmt.Println("1 - Добавить работника")
         fmt.Println("2 - Вывести среднюю зарплату работников")
-        fmt.Println("4 - Всех")
         fmt.Println("3 - Выход")
 
         var num string
@@ -69,8 +65,6 @@ func ChooseOption(num string, workerList *WorkerList) {
     case "2":
         CalculateAverageSalary(workerList)
     case "3":
-        ShowAll(workerList);
-    case "4":
         fmt.Println("Выход")
         os.Exit(0)
     default:
@@ -79,38 +73,7 @@ func ChooseOption(num string, workerList *WorkerList) {
 }
 
 func AddWorker(workerList *WorkerList) {
-    var newWorker Worker
-    fmt.Print("Введите имя работника: ")
-    fmt.Scanln(&newWorker.WORKER_NAME)
     
-    fmt.Print("Введите возраст работника: ")
-    fmt.Scanln(&newWorker.AGE)
-    
-    fmt.Print("Введите зарплату работника: ")
-    fmt.Scanln(&newWorker.SALARY)
-    
-    fmt.Print("Введите адрес работника: ")
-    fmt.Scanln(&newWorker.ADRESS)
-
-    _, err := db.Exec("INSERT INTO Worker (WORKER_NAME, AGE, SALARY, ADRESS) VALUES ($1, $2, $3, $4)",
-        newWorker.WORKER_NAME, newWorker.AGE, newWorker.SALARY, newWorker.ADRESS)
-    if err != nil {
-        fmt.Println("Ошибка при добавлении работника:", err)
-    } else {
-        fmt.Println("Работник успешно добавлен!")
-        workerList.Workers = append(workerList.Workers, newWorker)
-    }
-}
-
-func ShowAll(workerList *WorkerList) {
-    if len(workerList.Workers) == 0 {
-        fmt.Println("Список работников пуст.")
-    } else {
-        fmt.Println("Список всех работников:")
-        for _, worker := range workerList.Workers {
-            fmt.Printf("ID: %d, Имя: %s, Возраст: %d, Зарплата: %d, Адрес: %s\n", worker.ID_WORKER, worker.WORKER_NAME, worker.AGE, worker.SALARY, worker.ADRESS)
-        }
-    }
 }
 
 func CalculateAverageSalary(workerList *WorkerList) {
